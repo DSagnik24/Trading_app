b7e639788a459420cec1d063e9fb61d32f207eeb
Sagnik Dutta <sagnikduttaofficial18@gmail.com>
2025-10-13 21:41:04 +0530

    New Updates

--- src/main/java/com/Sagnik/Trading_App/Controller/AuthController.java
+++ src/main/java/com/Sagnik/Trading_App/Controller/AuthController.java
@@ -4,9 +4,9 @@ import com.Sagnik.Trading_App.Model.User;
 import com.Sagnik.Trading_App.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +18,7 @@ public class AuthController {
     private UserRepository userRepository;
 
     @PostMapping("/signUp")
-    public ResponseEntity<User> register(User user){
+    public ResponseEntity<User> register(@RequestBody User user){
 
         User newUser = new User();
         newUser.setEmail(user.getEmail());
