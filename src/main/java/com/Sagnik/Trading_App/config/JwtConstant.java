a6cf17e1223117c5d65114c798428719ee4d2f43
Sagnik Dutta <sagnikduttaofficial18@gmail.com>
2025-10-15 09:47:22 +0530

    Update Config file

---
+++ src/main/java/com/Sagnik/Trading_App/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.Sagnik.Trading_App.config;
+
+public class JwtConstant {
+    public static final String SECREAT_KEY ="rgahraetbahabtheathaeatjq46ybertW4Tw4twb";
+
+    public static final String JWT_HEADER="Authorization";
+
+}
